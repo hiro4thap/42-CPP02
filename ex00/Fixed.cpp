fc045de1d4839b7f2ffb77fac69f3d3dfd49b4ca
Hiroaki Ono <hiono@student.42adel.org.au>
2024-06-20 13:04:28 +0930

    added if statement for safe

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: hiono <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:20:30 by hiono             #+#    #+#             */
-/*   Updated: 2024/06/19 17:33:18 by hiono            ###   ########.fr       */
+/*   Updated: 2024/06/20 12:43:25 by hiono            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ Fixed::Fixed(const Fixed &rhs)
 Fixed&	Fixed::operator=(const Fixed &rhs)
 {
 	std::cout << "Copy assignment operator called" << "\n";
-	_fixed_point_value = rhs.getRawBits();
+	if (this != &rhs)
+		_fixed_point_value = rhs.getRawBits();
 	return *this;
 }
 
